a222911716796ddd1d923615b91bfb326af0c22c
lenovo <lenovo@WIN-1U3KRFBFLO9>
2017-10-17 22:27:14 +0800

    第一版

---
+++ src/com/feller/test/test.java
@@ -0,0 +1,11 @@
+package com.feller.test;
+
+public class test {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	     System.out.println("今天是星期一，开始学java");
+
+	}
+
+}
